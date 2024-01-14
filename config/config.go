0678167e2ea5faa0d202ea03d10f6311cf100393
HeesuKim0203 <heesukim998@gmail.com>
2024-01-14 23:17:04 +0900

    env setting

--- config/config.go
+++ config/config.go
@@ -20,12 +20,81 @@ type Config struct {
 var c *Config
 
 func init() {
-	err := godotenv.Load()
+	env, err := godotenv.Read("./.env")
 
 	if err != nil {
 		fmt.Printf("Not found .env File : ")
-		panic(err)
+		fmt.Printf("It fetches the specified environment variables, not from the .env file.")
+		specifiedEnv()
+	} else {
+		envFile(env)
 	}
+}
+
+func envFile(env map[string]string) {
+
+	c = &Config{}
+
+	discrodToken := env["DISCORD_PUBLIC_KEY"]
+
+	if discrodToken != "" {
+		c.discordToken = discrodToken
+	} else {
+		panic("Not found in .env file the 'DISCORD_PUBLIC_KEY'!")
+	}
+
+	youtubeToken := env["YOUTUBE_PUBLIC_KEY"]
+
+	if youtubeToken != "" {
+		c.youtubeToken = youtubeToken
+	} else {
+		panic("Not found in .env file the 'YOUTUBE_PUBLIC_KEY'!")
+	}
+
+	queueSize, err := strconv.Atoi(env["MUSIC_QUEUE_SIZE"])
+
+	if err != nil {
+		fmt.Println("Not correct format in .env file the 'MUSIC_QUEUE_SIZE'!")
+		fmt.Println("Use the default for 'MUSIC_QUUE_SIZE'.")
+		c.queueSize = 10
+	} else {
+		c.queueSize = queueSize
+	}
+
+	musicDuration, err := strconv.Atoi(env["MUSIC_DURATION"])
+
+	if err != nil {
+		fmt.Println("Not correct format in .env file the 'MUSIC_DURATION'!")
+		fmt.Println("Use the default for 'MUSIC_DURATION'.")
+		c.musicDuration = 480
+	} else {
+		c.musicDuration = musicDuration
+	}
+
+	botName := env["BOT_NAME"]
+
+	if botName != "" {
+		c.botName = botName
+	} else {
+		fmt.Println("Not found in .env file the 'BOT_NAME'!")
+		fmt.Println("Use the default for 'BOT_NAME'.")
+		c.botName = "!music"
+	}
+
+	searchListMax, err := strconv.Atoi(env["MAX_MUSIC_SEARCH_LIST"])
+
+	if err != nil {
+		fmt.Println("Not correct format in .env file the 'MAX_MUSIC_SEARCH_LIST'!")
+		fmt.Println("Use the default for 'MAX_MUSIC_SEARCH_LIST'.")
+		c.searchListMax = 5
+	} else {
+		c.searchListMax = searchListMax
+	}
+
+	fmt.Println("Config complete!")
+}
+
+func specifiedEnv() {
 
 	c = &Config{}
 
